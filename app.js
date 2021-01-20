88af451888c7118eb9cd154dca9e5ab4ac7e2523
nicholas jackson <nicdogg360@gmail.com>
2021-01-19 16:13:34 -0800

    basic functionality completed, need to get set size working

--- app.js
+++ app.js
@@ -1,32 +1,105 @@
-const layoutGrid = (width, height) => {
-    let grid = document.querySelector(".grid"); 
+let rgb_btn = document.querySelector(".rgb-btn");  
+let grey_btn = document.querySelector(".grey-btn");   
+let pen_color = document.querySelector(".color-picker")
+
+let reset_btn = document.querySelector(".reset-btn");  
+let size_btn = document.querySelector(".scroll-btn"); 
+let grid = document.querySelector(".grid");   
+
+
+const setPenColor = (e) =>{ 
+    if(e.target == rgb_btn){ 
+        setRGB(cellList);
+    } else if(e.target == grey_btn){
+        setGrey(cellList);
+    } else if( e.target == pen_color){
+        setPen(cellList);
+    }
+}
+const setSettings = (e) => {
+    if(e.target == reset_btn){
+        resetGrid(cellList);
+    } else if(e.target == size_btn ){ 
+        // TODO
+        console.log("reset btn"); 
+
+    }
+}
+
+const layoutGrid = (width = 16, height = 16) => { 
     grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${height}, 1fr)`;   
 
     let gridPop = (width * height);  
-    let cell = document.createElement("div"); 
-    
-    
     for(let i = 0; i <= gridPop; i++){  
         let cell = document.createElement("div"); 
-        cell.classList.add("cell");
-        grid.appendChild(cell); 
-    }
+            cell.classList.add("cell"); 
+            grid.appendChild(cell);  
+        }
+         let list = document.querySelectorAll('.cell');   
+         return list;
+} 
     
-    console.log(grid);  
+const resetGrid = (list) => {
 
+    list.forEach(cell => {
+        cell.style.backgroundColor = "white"; 
+
+    });
+} 
+const setRGB = (list) => {
+    list.forEach(cell => {
+        cell.addEventListener('mouseenter', () =>{
+            let red = Math.floor(Math.random() * 256);
+            let green = Math.floor(Math.random() * 256);
+            let blue = Math.floor(Math.random() * 256);
+            cell.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        })
+    })
+} 
+const setGrey = (list) => {
+    list.forEach(cell =>{
+        cell.addEventListener('mouseenter', () =>{
+            cell.style.backgroundColor = "grey";
+        })
+    })
+} 
+const setPen = (list) => {
+    list.forEach(cell =>{
+        cell.addEventListener('mouseenter', () =>{
+            cell.style.backgroundColor = pen_color.value;
+        })
+    })
 } 
 
+// TODO
+const setSizeReset = () => {
+    let size = prompt("Please enter your grid");  
+    resetGrid(cellList); 
+    grid.innerHTML = "";
+    layoutGrid(size,size);
+}
 
-layoutGrid(3,3); 
+    
+reset_btn.addEventListener('click',setSettings); 
+size_btn.addEventListener('click', setSettings);
+rgb_btn.addEventListener('click',setPenColor);
+grey_btn.addEventListener('click',setPenColor);  
+pen_color.addEventListener('click', setPenColor);
 
 
 
+let cellList = layoutGrid();    
 
 
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
